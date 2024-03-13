7c5266f2a5a329c98ec96929e45a0df3ee3ce10e
= <=>
2024-03-13 12:53:42 +0530

    bashscript added

--- list_ec2.sh
+++ list_ec2.sh
@@ -7,15 +7,15 @@ output_file="instances.csv"
 regions=$(aws ec2 describe-regions --query 'Regions[*].RegionName' --output text)
 
 # Create the CSV file with header
-echo "InstanceId,Region,Owner,PrivateIP, PublicIP, State, Tags" > "$output_file"
+echo "InstanceId,Region,Owner,PrivateIP, PublicIP, State" > "$output_file"
+
+# Define the tags you want to filter for
+tags="Name,Environment,Project"  # Example tags: Name, Environment, Project
 
 # Iterate over each region
 for region in $regions; do
     echo "Instances in region: $region"
-    # Run the command and append output to the CSV file
-    #aws ec2 describe-instances --region "$region" --query 'Reservations[*].Instances[*].[InstanceId, PublicIpAddress, PrivateIpAddress]' --output text | awk -v region="$region" -F'\t' '{OFS=","} {print region, $1, $2, $3}' >> "$output_file"
-    #echo "InstanceId,Region,Owner,PrivateIP,PublicIP,State,Tags" > "$output_file"
-    aws ec2 describe-instances --region "$region" --query 'Reservations[*].Instances[*].[InstanceId,Placement.AvailabilityZone,OwnerId,PrivateIpAddress,PublicIpAddress,State.Name,Tags]' --output text | awk -v region="$region" -F'\t' '{OFS=","} {print $1, region, $3, $4, $5, $6, $7}' >> "$output_file"
+    aws ec2 describe-instances --region "$region" --query "Reservations[*].Instances[*].[InstanceId,Placement.AvailabilityZone,OwnerId,PrivateIpAddress,PublicIpAddress,State.Name]" --output text | awk -v region="$region" -F'\t' '{OFS=","} {print $1, region, $3, $4, $5, $6, $7}' >> "$output_file"
 
     echo "----------------------------------------"
 done
