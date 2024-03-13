dffe1cfd4fd8a462bbfe9525c795863b6764bbae
= <=>
2024-03-13 12:33:16 +0530

    script file added

--- list_ec2.sh
+++ list_ec2.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Define the output CSV file
-output_file="ec2_instances.csv"
+output_file="instances.csv"
 
-# Write CSV header
-echo "InstanceId,Owner,PrivateIpAddress,PublicIpAddress,Region,State,Tags" > "$output_file"
+# Get a list of regions
+regions=$(aws ec2 describe-regions --query 'Regions[*].RegionName' --output text)
 
-# Get list of all regions
-regions=$(aws ec2 describe-regions --query "Regions[*].RegionName" --output text)
+# Create the CSV file with header
+echo "InstanceId,Region,Owner,PrivateIP, PublicIP, State, Tags" > "$output_file"
 
-# Loop through each region
+# Iterate over each region
 for region in $regions; do
-    # Get EC2 instances information for the current region
-    instances=$(aws ec2 describe-instances --region "$region" --output json)
+    echo "Instances in region: $region"
+    # Run the command and append output to the CSV file
+    #aws ec2 describe-instances --region "$region" --query 'Reservations[*].Instances[*].[InstanceId, PublicIpAddress, PrivateIpAddress]' --output text | awk -v region="$region" -F'\t' '{OFS=","} {print region, $1, $2, $3}' >> "$output_file"
+    #echo "InstanceId,Region,Owner,PrivateIP,PublicIP,State,Tags" > "$output_file"
+    aws ec2 describe-instances --region "$region" --query 'Reservations[*].Instances[*].[InstanceId,Placement.AvailabilityZone,OwnerId,PrivateIpAddress,PublicIpAddress,State.Name,Tags]' --output text | awk -v region="$region" -F'\t' '{OFS=","} {print $1, region, $3, $4, $5, $6, $7}' >> "$output_file"
 
-    # Extract required details and append to CSV
-    echo "$instances" | jq -r '.Reservations[] | .Instances[] | [.InstanceId, .OwnerId, .PrivateIpAddress, .PublicIpAddress, "'"$region"'", .State.Name, (.Tags | map(.Key + ":" + .Value) | join(", "))] | @csv' >> "$output_file"
+    echo "----------------------------------------"
 done
 
-echo "CSV file generated: $output_file"
+echo "Output written to $output_file"
